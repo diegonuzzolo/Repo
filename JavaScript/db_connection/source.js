be803e596f9757f65cee0b5ceda332947b1afd6f
diego <40766670+diegonuzzolo@users.noreply.github.com>
2024-04-24 22:21:10 +0200

    Delete JavaScript directory

--- JavaScript/db_connection/source.js
+++
@@ -1,5 +0,0 @@
-const mysql = require("mysql")
-
-const dbConfig = {
-    user: "nome"
-}
--- JavaScript/debug_test.js
+++
@@ -1,6 +0,0 @@
-const appiattimento = [[0, 1], [2, 3], [4, 5]].reduce((accumulatore, array) => {
-    debugger;
-    accumulatore.concat(array), []
-})
-
-console.log(appiattimento)
